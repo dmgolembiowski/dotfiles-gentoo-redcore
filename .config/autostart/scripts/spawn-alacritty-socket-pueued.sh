21d6a87b7886f5f51320e96357db628d955e7f5d
dmgolembiowski <dmgolembiowski@gmail.com>
2023-10-12 14:08:04 -0400

    Fixes typo

--- .config/autostart/scripts/spawn-alacritty-socket-pueued.sh
+++ .config/autostart/scripts/spawn-alacritty-socket-pueued.sh
@@ -1,5 +1,5 @@
 #!/bin/fish
 rm /var/run/user/1000/pueue_david.socket
-rm/var/run/user/1000/pueue.pid
+rm /var/run/user/1000/pueue.pid
 rm /home/david/.local/state/alacritty/alacritty-sock
 alacritty -T AlacrittyPueued --socket /home/david/.local/state/alacritty/alacritty-sock --hold --command pueued --verbose
